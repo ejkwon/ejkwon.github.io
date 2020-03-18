85238e9c1732b9981828522afc2ac8052a2bbe4c
ejkwon <girl5050y@gmail.com>
2020-03-18 19:16:47 +0900

    nice1

--- pj/nice1/resource/js/bam/zzz/cc/main.js
+++ pj/nice1/resource/js/bam/zzz/cc/main.js
@@ -9,11 +9,16 @@ $(function(){
      arrows: false
    });
    reHeight();
-
 });
 
 function reHeight(){
-  var vHeight = $('.visualSlider').outerHeight();
+  if(!$('body').hasClass('pc') && $(window).height() < 481){
+    $('.visualArea').css('height','80%');
+  } else{
+    $('.visualArea').css('height','');
+  }
+  var vHeight = $('.visualArea').outerHeight();
+  console.log(vHeight);
   $('.visualSlider .visual').css('height',vHeight+24);
   $('#container').css('padding-top',vHeight);
 }
